bb02361d87a81e3bf524326f2fb2f5d905d29bac
Muhamad Auli'a Ardani <muhamadardan997@gmail.com>
2025-09-17 12:29:29 +0700

    fix: correct vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Web-Porto-Ardani/', // <- sesuai nama repo
+  base: '/Web-Porto-Ardani/', // ← sesuai nama repo GitHub kamu
 })
