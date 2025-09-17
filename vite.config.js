424d576f6848080e080956184e686bf33d8c77db
Muhamad Auli'a Ardani <muhamadardan997@gmail.com>
2025-09-17 13:59:29 +0700

    Fix vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'Web-Porto-Ardani/', // ← sesuai nama repo GitHub kamu
+  base: '/Web-Porto-Ardani/', // ← sesuai nama repo GitHub kamu
 })
