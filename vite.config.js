fc529b600ed712af48c1195c7013d7908394f5da
Muhamad Auli'a Ardani <muhamadardan997@gmail.com>
2025-09-17 12:28:16 +0700

    fix: set correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/webportoardan/', // ganti sesuai nama repo kamu di GitHub
+  base: '/Web-Porto-Ardani/', // <- sesuai nama repo
 })
